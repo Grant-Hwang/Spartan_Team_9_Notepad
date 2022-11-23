7ecbc97ce6d1352c19088f0905e2fcfe6b6af5d0
rianbowgift <48279678+rianbowgift@users.noreply.github.com>
2022-11-23 19:06:28 +0900

    Update MemoList.java
    memoAdd를 추가하였고, 그에따라 생성자예시를 주석처리하였습니다.
    그외, 삭제부분에 memoDel_IndexSort라는 메소드를 만들어 삭제시 뒷부분 값들의 인덱스가 한개씩 줄어듭니다()

--- MemoList.java
+++ MemoList.java
@@ -7,18 +7,36 @@ import java.util.Scanner;
 public class MemoList {
     List<Memo> arr = new ArrayList<>();
     public MemoList() {
-        this.arr.add(new Memo( "1111", "여긴 내용입니다1"));
-        this.arr.add(new Memo("2222", "여긴 내용이에요2"));
-        this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
-        this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
-        this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
-        this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
+//         this.arr.add(new Memo( "1111", "여긴 내용입니다1"));
+//         this.arr.add(new Memo("2222", "여긴 내용이에요2"));
+//         this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
+//         this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
+//         this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
+//         this.arr.add(new Memo( "2222", "여긴 내용이에요2"));
 
     }
 
     @Override
     public String toString() {
         return this.arr.toString();
+    }
+    
+        public void memoAdd(int index, String name, String password){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm:ss");
+        String formatedNow = now.format(formatter);
+        Scanner Input = new Scanner(System.in);
+        System.out.println("이름을 입력해 주세요 ");
+        String named = Input.nextLine();
+        System.out.println("비밀번호를 입력해 주세요 ");
+        String pwd = Input.nextLine();
+        System.out.println("메모를 입력해 주세요 ");
+        String txt = Input.nextLine();
+
+        //삭제할때 index가 땡겨지므로 추가할땐 사이즈가 index
+        int size = arr.size();
+        this.arr.add(new Memo(named,pwd,txt,size));
+        this.arr.get(index).setDate(formatedNow);
     }
 
     public void memoEdit() {
@@ -67,6 +85,7 @@ public class MemoList {
             if (password.equals(this.arr.get(index).getPassword())) {
                 System.out.println("삭제한 메모내용 : " + this.idxGetText(index));
                 this.arr.remove(index);
+                memoDel_IndexSort(index);
             } else if (password != this.arr.get(index).getPassword()) {
                 System.out.println("비밀번호가 일치하지 않습니다.");
 
@@ -77,6 +96,14 @@ public class MemoList {
             System.out.println("수정할 메모가 존재하지 않습니다.");
         }
     }
+    
+    
+        public void memoDel_IndexSort(int index){
+        for(int i=index;i<arr.size();i++){
+            arr.get(i).one_minus_Index();
+        }
+    }
+    
 
     public String idxGetText(int index) {            //글의 수정 또는 삭제 시 글 번호를 넘겨 받고 리스트에서 글 번호에 해당하는  글 1건을 얻어와 리턴 시키는 메소드가 있다.
        return this.arr.get(index).text;
