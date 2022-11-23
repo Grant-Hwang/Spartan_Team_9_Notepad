5af2645d746c7e5b6a3a9c71f2e890ac6f5e1fc4
rianbowgift <48279678+rianbowgift@users.noreply.github.com>
2022-11-23 19:25:10 +0900

    Update Memo.java
    tostring을 조금 변경하였고, 생성자부분을 정상화시켰습니다

--- Memo.java
+++ Memo.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Memo {
-
+    private  int index;
     private String name;
      private String password;
     String text;
@@ -11,11 +11,12 @@ public class Memo {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm:ss");
     String formatedNow = now.format(formatter);
 
-    public Memo( String password, String text) {
-        this.name = "이름";
+    public Memo( String name,String password, String text,int index) {
+        this.name = name;
         this.password = password;
         this.text = text;
         this.date = this.formatedNow;
+        this.index = index;
     }
 
 
@@ -34,10 +35,13 @@ public class Memo {
     public void setDate(String date) {
         this.date = date;
     }
+    
+    public void one_minus_Index(){this.index = this.index-1;}
 
     @Override
     public String toString() {
-        return  "파일이름 : " + name +
+        return  "글번호 : " + index +
+                "\n작성자 이름 : " + name +
                 "\n작성일 : " + date +
                 "\ntext : " + text
                 ;
